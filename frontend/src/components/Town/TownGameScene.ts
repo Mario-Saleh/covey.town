c92a69c2e2bcdea39d6c230e9e7e6c5551ad31b5
Mario Saleh <mariosaleh66@gmail.com>
2026-09-08 21:45:56 -0400

    Change background of 'You' text to green

--- frontend/src/components/Town/TownGameScene.ts
+++ frontend/src/components/Town/TownGameScene.ts
@@ -421,7 +421,7 @@ export default class TownGameScene extends Phaser.Scene {
         font: '18px monospace',
         color: '#000000',
         // padding: {x: 20, y: 10},
-        backgroundColor: '#ffffff',
+        backgroundColor: '#02e120',
       })
       .setDepth(6);
     this.coveyTownController.ourPlayer.gameObjects = {
